1856160feeb33c06f680c78869643e5ef8c67bb4
Paul Hoffart <pahoffart25@gmail.com>
2019-02-03 04:26:21 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -7,7 +7,8 @@ var greeting = 'Hello, everybody!'
 it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
   expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
 })
-
+var special = 'Hello ${special}!'
 it('interpolates a string in `conversation`', function() {
   expect(conversation).toEqual(`Let's talk about ${topic}.`)
 })
+var topic = 'Let\'s talk about ${science}.'
